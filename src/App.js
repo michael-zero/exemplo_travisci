01c5fee401315d682dc38c450e1b450fdf0d77cf
michael <mlmbufpi@gmail.com>
2021-06-04 20:19:09 -0300

    na luta

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Tentando a build no Travis
+         Tentando a build no Travis.... de novo
         </p>
         <a
           className="App-link"
